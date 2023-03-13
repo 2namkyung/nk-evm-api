0e6ea5fabe82b76c4e46687f80a4824b306fac2f
2namkyung <iddbxk@daum.net>
2023-03-13 18:44:47 +0900

    chore: node typescript project & event-signature erc721 settings

---
+++ src/event-signature/erc721.ts
@@ -0,0 +1,138 @@
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+export const ERC721_TRANSFER_EVENT_SIGNATURE =
+  '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
+export const ERC721_TRANSFER_EVENT_ABI = [
+  {
+    indexed: true,
+    name: 'from',
+    type: 'address',
+  },
+  {
+    indexed: true,
+    name: 'to',
+    type: 'address',
+  },
+  {
+    indexed: true,
+    name: 'tokenId',
+    type: 'uint256',
+  },
+];
+
+export const ERC721_TOKENURI_ABI = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'tokenURI',
+    outputs: [
+      {
+        name: '',
+        type: 'string',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+export const ERC721_OWNEROF_ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'ownerOf',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+export const SUPPORTS_INTERFACE = [
+  {
+    inputs: [
+      {
+        internalType: 'bytes4',
+        name: 'interfaceId',
+        type: 'bytes4',
+      },
+    ],
+    name: 'supportsInterface',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+export const ERC2981_ROYALTY_INFO = [
+  {
+    inputs: [
+      {
+        internalType: 'bytes4',
+        name: 'interfaceId',
+        type: 'bytes4',
+      },
+    ],
+    name: 'supportsInterface',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_tokenId',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: '_salePrice',
+        type: 'uint256',
+      },
+    ],
+    name: 'royaltyInfo',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
