aaea4112d137acce8928e062831b5fa8b3935eaa
namkyung.lee <iddbxk@daum.net>
2023-07-06 12:17:02 +0900

    collector:erc721 refactoring

--- src/evm/erc721/index.ts
+++ src/evm/erc721/index.ts
@@ -9,51 +9,79 @@ import { insertERC721TransferTxInfo } from './query';
 const abiCoder = new ethers.AbiCoder();
 
 export async function findERC721TransferEventInBlock() {
-  webSocketProvider.on('block', async (newBlockNumber: number) => {
+  webSocketProvider.on('block', getBlockLog);
+}
+
+async function getBlockLog(newBlockNumber: number) {
+  try {
     const block = await jsonRpcProvider.getBlock(newBlockNumber);
+    await filterNftTransferLogInBlock(block);
+  } catch (error) {
+    console.log('[ERROR: GET BLOCK]', error);
+  }
+}
 
-    block.transactions.forEach(async (txHash) => {
-      try {
-        const receipt = await jsonRpcProvider.getTransactionReceipt(txHash);
-        if (receipt && receipt.status && receipt.logs.length !== 0) {
-          receipt.logs.forEach(async (log) => {
-            const { topics } = log;
-            const contract = log.address;
-
-            if (
-              topics[0] === ERC721_TRANSFER_EVENT_SIGNATURE &&
-              topics.length === 4
-            ) {
-              const from = abiCoder.decode(['address'], topics[1])[0];
-              const to = abiCoder.decode(['address'], topics[2])[0];
-              const tokenId = abiCoder
-                .decode(['uint256'], topics[3])[0]
-                .toString();
-              const eventName = from === ZERO_ADDRESS ? 'MINT' : 'TRANSFER';
-              const { value } = await jsonRpcProvider.getTransaction(txHash);
-              const price = ethers.formatEther(value);
-              console.log(price);
-
-              const nftData = {
-                contract,
-                tokenId,
-                from,
-                to,
-                txHash,
-                eventName,
-                price,
-              };
-
-              // await insertERC721TransferTxInfo(nftData);
-            }
-          });
-        }
-      } catch (error) {
-        console.log('[ERROR : GET BLOCK', error);
-        return error;
+async function filterNftTransferLogInBlock(block: ethers.Block) {
+  for (const txHash of block.transactions) {
+    try {
+      const receipt = await jsonRpcProvider.getTransactionReceipt(txHash);
+      if (receipt?.status && receipt.logs.length !== 0) {
+        await processTransactionLogs(txHash, receipt.logs);
       }
-    });
-  });
+    } catch (error) {
+      console.log('[ERROR: PROCESS TRANSACTION]', error);
+    }
+  }
+}
+
+async function processTransactionLogs(
+  txHash: string,
+  logs: readonly ethers.Log[],
+) {
+  try {
+    for (const log of logs) {
+      const { topics } = log;
+      const contract = log.address;
+
+      if (
+        topics[0] === ERC721_TRANSFER_EVENT_SIGNATURE &&
+        topics.length === 4
+      ) {
+        await saveNftTransferTx(txHash, contract, topics);
+      }
+    }
+  } catch (error) {
+    console.log('[ERROR: PROCCESS TRANSACTION]', error);
+  }
+}
+
+async function saveNftTransferTx(
+  txHash: string,
+  contract: string,
+  topics: readonly string[],
+) {
+  const from = abiCoder.decode(['address'], topics[1])[0];
+  const to = abiCoder.decode(['address'], topics[2])[0];
+  const tokenId = abiCoder.decode(['uint256'], topics[3])[0].toString();
+  const eventName = from === ZERO_ADDRESS ? 'MINT' : 'TRANSFER';
+  const { value } = await jsonRpcProvider.getTransaction(txHash);
+  const price = ethers.formatEther(value);
+
+  const nftData = {
+    contract,
+    tokenId,
+    from,
+    to,
+    txHash,
+    eventName,
+    price,
+  };
+
+  try {
+    await insertERC721TransferTxInfo(nftData);
+  } catch (error) {
+    console.log('[ERROR: INSERT ERC721 TRANSFER INFO]', error);
+  }
 }
 
 findERC721TransferEventInBlock();
