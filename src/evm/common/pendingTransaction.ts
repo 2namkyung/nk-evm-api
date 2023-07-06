62cc8c026dd40a8e3434d7bdd90b871cec397b6a
namkyung.lee <iddbxk@daum.net>
2023-07-06 12:26:22 +0900

    pendingTransaction refactoring

--- src/evm/common/pendingTransaction.ts
+++ src/evm/common/pendingTransaction.ts
@@ -1,10 +1,17 @@
 import { jsonRpcProvider, webSocketProvider } from '../../utils/ethers';
 
 async function listenPendingTx() {
-  webSocketProvider.on('pending', async (txHash: string) => {
+  webSocketProvider.on('pending', getPendingLog);
+}
+
+async function getPendingLog(txHash: string) {
+  try {
     const pendingTx = await jsonRpcProvider.getTransaction(txHash);
     console.log(pendingTx);
-  });
+    return pendingTx;
+  } catch (error) {
+    console.log('[ERROR: GET PENDING LOG]', error);
+  }
 }
 
 listenPendingTx();
