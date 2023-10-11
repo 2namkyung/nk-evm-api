80a62fc4bf18b1545ef85968ad656c803bc7ee13
namkyung.lee <iddbxk@daum.net>
2023-10-11 22:41:03 +0900

    refactoring:listenPendingTx async -> then syntax

--- src/evm/common/pendingTransaction.ts
+++ src/evm/common/pendingTransaction.ts
@@ -1,17 +1,11 @@
 import { jsonRpcProvider, webSocketProvider } from '../../utils/ethers';
 
 async function listenPendingTx() {
-  webSocketProvider.on('pending', getPendingLog);
-}
-
-async function getPendingLog(txHash: string) {
-  try {
-    const pendingTx = await jsonRpcProvider.getTransaction(txHash);
-    console.log(pendingTx);
-    return pendingTx;
-  } catch (error) {
-    console.log('[ERROR: GET PENDING LOG]', error);
-  }
+  webSocketProvider.on('pending', (txHash: string) => {
+    jsonRpcProvider.getTransaction(txHash).then((transaction) => {
+      console.log(transaction);
+    });
+  });
 }
 
 listenPendingTx();
